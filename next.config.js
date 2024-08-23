cd03289b776f6a4fd5df40b1e86871e18e344610
ahnshy <ahnshy@naver.com>
2024-08-24 06:52:23 +0900

    <fix> : ignoreBuildErrors: remove true property <fix> : add exclude in typescript config

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   async headers() {
     return [
       {
